8b8fab5eb77c344710f8007fa938d7832a1c91aa
ilearnblogger <ilearnblogger@users.noreply.github.com>
2015-04-19 22:22:10 +0800

    Update geturl.py

--- geturl.py
+++ geturl.py
@@ -1,3 +1,6 @@
+# Code explaination: http://ilearnblogger.blogspot.tw/2015/04/qnaplinux-python-programming-01-setup.html
+#!/usr/bin/python
+
 import urllib
 urls=('http://ilearnblogger.blogspot.tw/2015/04/qnaplinux-tool-postgresql-from-9210.html','http://ilearnblogger.blogspot.tw/2015/03/qnaplinux-tool-postgresql-database.html')
 i = 0
