bce13e4a2cba7cd307a101a414e32704831bb297
ilearnblogger <ilearnblogger@gmail.com>
2015-04-26 19:46:48 +0800

    Final for blog 04

--- extractDOM.py
+++ extractDOM.py
@@ -1,8 +1,18 @@
-# Code explaination: http://ilearnblogger.blogspot.tw/2015/04/qnaplinux-python-programming-01-setup.html
+# Code explaination: Parse HTML and Extract texts defined in some XPath
+# http://ilearnblogger.blogspot.tw/2015/04/qnaplinux-python-programming-04-learn.html
 #!/usr/bin/python
 
 import urllib3
+from bs4 import BeautifulSoup
 
 http = urllib3.PoolManager()
-r = http.request('GET', 'http://ilearnblogger.blogspot.tw/2015/04/qnaplinux-python-programming-01-setup.html')
-print r.status, r.data
+r = http.request('GET', 'http://ilearnblogger.blogspot.tw/2014/01/office-onenote.html')
+
+soup = BeautifulSoup(r.data)
+items = soup.select(".post-labels")
+tags = []
+for item in items: 
+	for links in item.find_all('a'): 
+		tags.append(links.string.strip())
+print tags
+for s in tags: print s
